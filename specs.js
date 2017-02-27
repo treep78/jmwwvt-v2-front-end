058e4af61ccc5f3243dea3f2db412db612ff6da4
treep78 <treep78@gmail.com>
2017-02-27 17:50:40 -0500

    build for deploy

--- specs.js
+++ specs.js
@@ -8,16 +8,16 @@ webpackJsonp([1],{
 	// Load all specs so webpack can find them. Think of this as an automatic
 	// manifest for bundling specs.
 
-	var req = __webpack_require__(28);
+	var req = __webpack_require__(26);
 	req.keys().forEach(req);
 
 /***/ },
 
-/***/ 28:
+/***/ 26:
 /***/ function(module, exports, __webpack_require__) {
 
 	var map = {
-		"./example.spec.js": 29
+		"./example.spec.js": 27
 	};
 	function webpackContext(req) {
 		return __webpack_require__(webpackContextResolve(req));
@@ -30,17 +30,17 @@ webpackJsonp([1],{
 	};
 	webpackContext.resolve = webpackContextResolve;
 	module.exports = webpackContext;
-	webpackContext.id = 28;
+	webpackContext.id = 26;
 
 
 /***/ },
 
-/***/ 29:
+/***/ 27:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var example = __webpack_require__(30);
+	var example = __webpack_require__(28);
 
 	describe('Example', function () {
 	  it('is true', function () {
@@ -50,7 +50,7 @@ webpackJsonp([1],{
 
 /***/ },
 
-/***/ 30:
+/***/ 28:
 /***/ function(module, exports) {
 
 	'use strict';
