1acb733c90b50925c9a128fdcb6f027097b9b5cd
treep78 <treep78@gmail.com>
2017-02-27 17:21:59 -0500

    prep for delpoy

--- specs.js
+++ specs.js
@@ -8,16 +8,16 @@ webpackJsonp([1],{
 	// Load all specs so webpack can find them. Think of this as an automatic
 	// manifest for bundling specs.
 
-	var req = __webpack_require__(22);
+	var req = __webpack_require__(28);
 	req.keys().forEach(req);
 
 /***/ },
 
-/***/ 22:
+/***/ 28:
 /***/ function(module, exports, __webpack_require__) {
 
 	var map = {
-		"./example.spec.js": 23
+		"./example.spec.js": 29
 	};
 	function webpackContext(req) {
 		return __webpack_require__(webpackContextResolve(req));
@@ -30,17 +30,17 @@ webpackJsonp([1],{
 	};
 	webpackContext.resolve = webpackContextResolve;
 	module.exports = webpackContext;
-	webpackContext.id = 22;
+	webpackContext.id = 28;
 
 
 /***/ },
 
-/***/ 23:
+/***/ 29:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var example = __webpack_require__(24);
+	var example = __webpack_require__(30);
 
 	describe('Example', function () {
 	  it('is true', function () {
@@ -50,7 +50,7 @@ webpackJsonp([1],{
 
 /***/ },
 
-/***/ 24:
+/***/ 30:
 /***/ function(module, exports) {
 
 	'use strict';
